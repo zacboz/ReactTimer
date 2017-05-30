490afe69d6fe688e16c8c66ee0558a830350a72c
zacboz <zacboz@gmail.com>
2017-05-30 14:54:08 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      applicationStyles: 'app/styles/app.scss'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
+
+
+// Due to a library bug there is a small issue in the next video. In the next lecture
+// you'll learn how to setup source maps by setting a "devtool" property
+// in webpack.config.js. In the lecture I set the value to
+// "cheap-module-eval-source-map". This might cause the source maps to not work
+//  as shown in the video.
+//
+// If you are getting this error, try setting the value to either
+//  "inline-source-map" or "eval-source-map" instead.
