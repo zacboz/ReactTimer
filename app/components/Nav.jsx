32f4fc068e747b9f04ff83a1f5e8e970e63287b6
zacboz <zacboz@gmail.com>
2017-05-31 10:58:04 -0600

    add navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,10 +8,10 @@ var Nav = () => {
           <ul className='menu'>
             <li className='menu-text'>React Timer App</li>
             <li>
-              <IndexLink to="/" activeClassName='active-link' activeStyle={{fontWeight: "bold"}}>Timer</IndexLink>
+              <IndexLink to="/" activeClassName='active-link'>Timer</IndexLink>
             </li>
             <li>
-              <Link to="/" activeClassName='active-link' activeStyle={{fontWeight: "bold"}}>Countdown</Link>
+              <Link to="/countdown" activeClassName='active-link'>Countdown</Link>
             </li>
           </ul>
         </div>
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Render Timer Component</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Timer;
