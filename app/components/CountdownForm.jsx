5b8c8e72205e6cf4935af740ac553d5a322a13ee
zacboz <zacboz@gmail.com>
2017-06-01 00:17:49 -0600

    support foundation settings

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,6 +5,8 @@ var CountdownForm = React.createClass({
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
 
+    
+
     if (strSeconds.match(/^[0-9]*$/)) {
       this.refs.seconds.value = '';
       this.props.onSetCountdown(parseInt(strSeconds, 10));
